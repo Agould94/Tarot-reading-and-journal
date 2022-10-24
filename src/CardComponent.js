a62f99d7f60b13a17aa455e014e61a2dc5907912
Agould94 <77901585+Agould94@users.noreply.github.com>
2022-10-24 16:27:04 -0400

    added card component

---
+++ src/CardComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CardComponent(){
+    return(
+        <div>
+            <h1>A card will go here.</h1>
+        </div>
+    )
+}
+
+export default CardComponent;
