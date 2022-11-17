f5ac38743e17dc4f8f6982489b6f502027561e1d
Agould94 <77901585+Agould94@users.noreply.github.com>
2022-11-17 16:35:17 -0500

    changed card card

--- src/components/CardCard.js
+++ src/components/CardCard.js
@@ -2,14 +2,9 @@ import { click } from "@testing-library/user-event/dist/click";
 import React, {useState} from "react";
 
 function CardCard({card}){
-    const [clicked, setClicked] = useState(false)
-
-    function handleCardClick(){
-        setClicked(!clicked)
-    }
 
     return(
-        <div onClick = {handleCardClick} className = "col-sm" align="center">
+        <div  className = "col-sm" align="center">
             <div className = "card m-2" style = {{width: '18rem'}}>
                 <img src = {require(`../cards/${card.img}`)} className = "card-img-top"></img>
                 <p>{card.name}</p>
